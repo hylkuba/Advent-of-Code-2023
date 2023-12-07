836b3c4d78ee84f3afb01e0302604cf6f65d87cb
Jakub Hýl <hyljaku2@fit.cvut.cz>
2023-12-07 20:27:43 +0100

    Day6: completed 2nd star

--- Day6-WaitForIt.cpp
+++ Day6-WaitForIt.cpp
@@ -29,38 +29,44 @@ void readFile(std::vector<std::string> &lines) {
     }
 }
 
-void printMap(const std::map<int, std::pair<int, int>> &myMap) {
+void printMap(const std::map<size_t, std::pair<size_t, size_t>> &myMap) {
     for(const auto &element : myMap) {
         std::cout << "Key: " << element.first << " => (time: " <<  element.second.first <<
         " | distance: " << element.second.second << ")" << std::endl;
     }
 }
 
-void storeData(const std::vector<std::string> &lines, std::map<int, std::pair<int, int>> &record) {
+void storeData(const std::vector<std::string> &lines, std::map<size_t, std::pair<size_t, size_t>> &record, size_t &totalTime, size_t &totalDistance) {
     for (const auto &line : lines) {
         std::istringstream iss(line);
         std::string word;
         iss >> word;
-        int count = 0;
+        size_t count = 0;
         if(word == "Time:") {
+            std::string number = "";
             while(iss >> word) {
-                record[count] = std::make_pair(stoi(word), 0); 
+                record[count] = std::make_pair(stoul(word), 0); 
                 count++;
+                number += word;
             }
+            totalTime = stoull(number);
         } else if(word == "Distance:") {
+            std::string number = "";
             while(iss >> word) {
-                record[count] = std::make_pair(record[count].first, stoi(word)); 
+                record[count] = std::make_pair(record[count].first, stoul(word)); 
                 count++;
+                number += word;
             }
+            totalDistance = stoull(number);
         }
     }
 }
 
-int calculate(int time, int recordToBeat) {
-    int result = 0;
+size_t calculate(size_t time, size_t recordToBeat) {
+    size_t result = 0;
 
     // i = button hold time
-    for (int i = 1; i < time; i++) {
+    for (size_t i = 1; i < time; i++) {
         if(i * (time - i) > recordToBeat) {
             result++;
         }
@@ -69,8 +75,8 @@ int calculate(int time, int recordToBeat) {
     return result;
 }
 
-int beatRecord(const std::map<int, std::pair<int, int>> &record) {
-    int sum = 1;
+size_t beatRecord(const std::map<size_t, std::pair<size_t, size_t>> &record) {
+    size_t sum = 1;
     for(const auto &element : record) {
         sum *= calculate(element.second.first, element.second. second);
     }
@@ -82,11 +88,11 @@ int main(void) {
 
     readFile(lines);
 
-    std::map<int, std::pair<int, int>> record;
+    std::map<size_t, std::pair<size_t, size_t>> record;
 
-    storeData(lines, record);
-
-    //printMap(record);
+    size_t totalTime, totalDistance;
+    storeData(lines, record, totalTime, totalDistance);
 
     std::cout << "Number of ways to beat Record: " << beatRecord(record) << std::endl;
+    std::cout << "Number of ways to beat Record of complete time: " << calculate(totalTime, totalDistance) << std::endl;
 }

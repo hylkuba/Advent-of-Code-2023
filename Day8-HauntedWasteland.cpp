27dfd46cf6860adffface5345d342ca460ca527d
Jakub Hýl <hyljaku2@fit.cvut.cz>
2023-12-09 19:17:59 +0100

    Day8: storing data

--- Day8-HauntedWasteland.cpp
+++ Day8-HauntedWasteland.cpp
@@ -28,10 +28,69 @@ void readFile(std::vector<std::string> &lines) {
     }
 }
 
+void printMap(const std::map<std::string, std::pair<std::string, std::string>> &myMap) {
+    for(const auto &pair : myMap) {
+        std::cout << pair.first << " = (" << pair.second.first << " ," << pair.second.second << ")" << std::endl;
+    }
+}
+
+void storeData(std::vector<std::string> &lines, std::vector<char> &directions,
+    std::map<std::string, std::pair<std::string, std::string>> &network) {
+
+    int lineCnt = 0;
+    for(const auto &line : lines) {
+        std::istringstream iss(line);
+        std::string word;
+        iss >> word;
+
+        if(lineCnt == 0) {
+            for (size_t i = 0; i < word.length(); i++) {
+                directions.push_back(word[i]);
+            }
+            lineCnt++;
+            continue;
+        } else if(lineCnt == 1) {
+            lineCnt++;
+            continue;
+        }
+
+        std::string key = word, pair1, pair2;
+        // '='
+        iss >> word;
+        // '(key'
+        iss >> word;
+        if (0 != std::string::npos && 4 != std::string::npos) {
+            pair1 = word.substr(1, 3);
+        }
+        iss >> word;
+        if (0 != std::string::npos && 3 != std::string::npos) {
+            pair2 = word.substr(0, 2);
+        }
+        network[key] = std::make_pair(pair1, pair2);
+    }
+}
+
 int main(void) {
     std::vector<std::string> lines;
 
+    std::vector<char> directions;
+    /**
+     * @brief key is a string, value is pair of (left, right)
+     * 
+     */
+    std::map<std::string, std::pair<std::string, std::string>> network;
+    
     readFile(lines);
+
+    storeData(lines, directions, network);
+
+    /*std::cout << "Printing network map:" << std::endl;
+    printMap(network);
+    
+    std::cout << "Printing vector:" << std::endl;
+    for(const auto &c : directions) {
+        std::cout << c << ", ";
+    }*/
 
     return 0;
 }

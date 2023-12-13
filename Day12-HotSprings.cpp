dac2d683e3c95ce37fc0032bb93c3b5f07fb964c
Jakub Hýl <hyljaku2@fit.cvut.cz>
2023-12-13 18:54:19 +0100

    Day12: input

---
+++ Day12-HotSprings.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+#define FILE "Day11-input.txt"
+
+/**
+ * @brief Reads file defined as FILE
+ * 
+ * @param lines vector of strings where all lines of file will be stored
+ */
+void readFile(std::vector<std::string> &lines) {
+
+    std::ifstream inputFile(FILE);
+    
+    if (inputFile.is_open()) {
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            lines.push_back(line);
+        }
+        inputFile.close();
+    } else {
+        std::cerr << "Unable to open the file." << std::endl;
+    }
+}
+
+int main(void) {
+    std::vector<std::string> lines;
+
+    readFile(lines);
+
+    return 0;
+}

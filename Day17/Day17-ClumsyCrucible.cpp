91736d523b9d707a31c0e1c2d70946e2b435cc99
Jakub Hýl <hyljaku2@fit.cvut.cz>
2023-12-19 18:03:44 +0100

    storing input

--- Day17/Day17-ClumsyCrucible.cpp
+++ Day17/Day17-ClumsyCrucible.cpp
@@ -27,10 +27,47 @@ void readFile(std::vector<std::string> &lines) {
     }
 }
 
+void printBlocks(
+        std::map<std::pair<int, int>, int> &blocks,
+        std::pair<int, int> dimensions) {
+    
+    for (int y = 1; y <= dimensions.second; y++) {
+        for (int x = 1; x < dimensions.first; x++) {
+            std::cout << blocks[std::make_pair(x, y)];
+        }
+        std::cout << std::endl;
+    }
+}
+
+std::pair<int, int> storeBlocks(
+        std::vector<std::string> &lines,
+        std::map<std::pair<int, int>, int> &blocks) {
+    
+    int width = 0, y = 1;
+    for(const auto &line : lines) {
+        if(y == 1) {
+            width = line.length();
+        }
+
+        for (size_t x = 1; x <= line.length(); x++) {
+            blocks[std::make_pair(x, y)] = line[x] - 48;
+        }
+        y++;
+    }
+
+    return std::make_pair(width, y - 1);
+}
+
 int main(void) {
     std::vector<std::string> lines;
 
     readFile(lines);
+
+    std::map<std::pair<int, int>, int> blocks;
+
+    std::pair<int, int> dimensions = storeBlocks(lines, blocks);
+
+    printBlocks(blocks, dimensions);
 
     return 0;
 }

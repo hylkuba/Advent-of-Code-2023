413351957184e08c0cf293788fa7c1517d98d76f
Jakub Hýl <hyljaku2@fit.cvut.cz>
2023-12-20 13:40:12 +0100

    not considering 3 consecutive steps

--- Day17/Day17-ClumsyCrucible.cpp
+++ Day17/Day17-ClumsyCrucible.cpp
@@ -65,15 +65,13 @@ int findLowestSumPath(std::map<std::pair<int, int>, int>& blocks, std::pair<int,
     std::queue<std::pair<int, int>> q;
     std::map<std::pair<int, int>, int> values;
     std::set<std::pair<int, int>> visited;
-    std::map<std::pair<int, int>, std::pair<int, int>> parents; // Keep track of parents
+    std::map<std::pair<int, int>, std::pair<int, int>> parents;
 
-    // Start from (1, 1)
     std::pair<int, int> start = std::make_pair(1, 1);
     q.push(start);
     values[start] = blocks[start];
     visited.insert(start);
 
-    // Possible moves: right, down, left, up
     int dx[] = {1, 0, -1, 0};
     int dy[] = {0, 1, 0, -1};
 
@@ -81,20 +79,16 @@ int findLowestSumPath(std::map<std::pair<int, int>, int>& blocks, std::pair<int,
         std::pair<int, int> current = q.front();
         q.pop();
 
-        // Explore neighbors
         for (int i = 0; i < 4; ++i) {
             int nx = current.first + dx[i];
             int ny = current.second + dy[i];
 
-            // Check if the neighbor is within bounds
             if (nx >= 1 && nx <= dimensions.first && ny >= 1 && ny <= dimensions.second &&
                 (visited.find(std::make_pair(nx, ny)) == visited.end())) {
 
-                // Calculate the sum of values for the path to this neighbor
                 int sum = values[current] + blocks[std::make_pair(nx, ny)];
                 std::pair<int, int> newPair = std::make_pair(nx, ny);
 
-                // If the neighbor is not visited or the new path has a lower sum
                 if (visited.find(newPair) == visited.end() || sum < values[newPair]) {
                     q.push(newPair);
                     values[newPair] = sum;
@@ -105,8 +99,7 @@ int findLowestSumPath(std::map<std::pair<int, int>, int>& blocks, std::pair<int,
         }
     }
 
-    // Reconstruct the path
-    /*std::pair<int, int> current = dimensions;
+    std::pair<int, int> current = dimensions;
     std::vector<std::pair<int, int>> path;
 
     while (current != start) {
@@ -117,15 +110,13 @@ int findLowestSumPath(std::map<std::pair<int, int>, int>& blocks, std::pair<int,
     path.push_back(start);
     std::reverse(path.begin(), path.end());
 
-    // Print the reconstructed path
     std::cout << "Reconstructed Path: ";
     for (const auto& point : path) {
         std::cout << "(" << point.first << ", " << point.second << ") ";
     }
-    std::cout << std::endl;*/
+    std::cout << std::endl;
 
-    // Return the sum at the destination or -1 if no path is found
-    return values.count(dimensions) ? values[dimensions] : -1;
+    return values[dimensions];
 }
 
 int main(void) {

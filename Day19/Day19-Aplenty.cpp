a6820df07ebd5182da00936061e590b469c1c659
Jakub Hýl <hyljaku2@fit.cvut.cz>
2023-12-23 17:03:39 +0100

    dividing input

--- Day19/Day19-Aplenty.cpp
+++ Day19/Day19-Aplenty.cpp
@@ -26,10 +26,80 @@ void readFile(std::vector<std::string> &lines) {
     }
 }
 
+class CPart {
+
+    CPart() : m_x(0), m_m(0), m_a(0), m_s(0) {
+        
+    }
+
+    int m_x;
+    int m_m;
+    int m_a;
+    int m_s;
+
+    friend std::ostream& operator<<(std::ostream& os, const CPart& obj) {
+        os << "m_x: " << obj.m_x << ", "
+        << "m_m: " << obj.m_m << ", "
+        << "m_a: " << obj.m_a << ", "
+        << "m_s: " << obj.m_s;
+        return os;
+    }
+};
+
+struct TRule {
+    std::string m_name;
+    char m_sign;
+    int m_value;
+    std::string posOutcome;
+    std::string negativeOutcome;    // null string if no negative outcome
+
+    friend std::ostream& operator<<(std::ostream& os, const TRule& rule) {
+        os << "Name: " << rule.m_name << ", "
+        << "Sign: " << rule.m_sign << ", "
+        << "Value: " << rule.m_value << ", "
+        << "Positive Outcome: " << rule.posOutcome << ", "
+        << "Negative Outcome: " << rule.negativeOutcome;
+        return os;
+    }
+};
+
+void storePart(std::string line, std::vector<CPart> &parts) {
+
+}
+
+void storeWorkflow(std::string line, std::map<std::string, std::vector<TRule>> &workflows) {
+
+}
+
+void storeInput(
+        std::vector<std::string> &lines,
+        std::vector<CPart> &parts,
+        std::map<std::string, std::vector<TRule>> &workflows) {
+    
+    bool rules = true;
+    for(auto &line : lines) {
+        if(line == "") {
+            rules = false;
+            continue;
+        }
+
+        if(rules) {
+            storePart(line, parts);
+        } else {
+            storeWorkflow(line, workflows);
+        }
+    }
+}
+
 int main(void) {
     std::vector<std::string> lines;
 
     readFile(lines);
+
+    std::vector<CPart> parts;
+    std::map<std::string, std::vector<TRule>> workflows;
+
+    storeInput(lines, parts, workflows);
 
     return 0;
 }

5f31bee644a01da1c48e583f43b354d38f0d25ef
Jakub Hýl <hyljaku2@fit.cvut.cz>
2023-12-25 20:20:04 +0100

    input

--- Day20/Day20-PulsePropagation.cpp
+++ Day20/Day20-PulsePropagation.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <vector>
+#include <map>
+#include <algorithm>
 
 #define FILE "Day19-input.txt"
 
@@ -25,10 +28,102 @@ void readFile(std::vector<std::string> &lines) {
     }
 }
 
+/*
+    ! FLIP FLOP '%'
+        * Either ON or OFF
+        * HIGH Pulse - NOTHING
+        * LOW Pulse - SENDS: OFF to ON -> HIGH PULSE
+        *                    ON to OFF -> LOW PULSE
+ */
+
+/*
+    ! CONJUNCTION '&'
+        * Remember the type of the most recent pulse received
+            * from each of their connected input modules
+        ? DEFAULT - low pulse
+        * When a pulse is received, the conjunction module first
+            * updates its memory for that input
+        * THEN SENDS: HIGH -> LOW
+        *             LOW -> HIGH
+*/
+
+/*
+    ! BROADCASTER
+    * Sends same signal it recieves to all of its directions
+*/
+
+/*
+    ! BUTTON MODULE
+    * When pressed, LOW signal sent to BROADCASTER
+*/
+
+/*
+    * Pulses are always processed in the order they are sent.
+    * So, if a pulse is sent to modules a, b, and c, and then
+    * module a processes its pulse and sends more pulses, the
+    * pulses sent to modules b and c would have to be handled first
+*/
+
+struct TType{
+    char type;
+    std::string name;
+    
+    // FLIP FLOP
+    bool status;
+
+    // CONJUNCTION
+    bool recentPulse;
+};
+
+void storeModules(
+        std::vector<std::string> &lines,
+        std::map<std::string, TType> &modules,
+        std::vector<std::string> &broadcaster) {
+
+    for(const auto &line : lines) {
+        std::istringstream iss(line);
+        std::string currWord;
+        iss >> currWord;
+
+        if(currWord == "broadcaster") {
+            // Skip the "->"
+            iss >> currWord;
+
+            // Extract each string value separately into the vector
+            while (iss >> currWord) {
+                // Remove commas from the current word
+                currWord.erase(std::remove_if(currWord.begin(), currWord.end(),
+                    [](char c) { return c == ','; }), currWord.end());
+                
+                // Store the current word in the vector
+                broadcaster.push_back(currWord);
+            }
+        }
+    }
+}
+
+size_t sumTotal(
+        std::vector<std::string> &lines,
+        std::map<std::string, TType> &modules) {
+
+}
+
 int main(void) {
     std::vector<std::string> lines;
 
     readFile(lines);
+
+    std::map<std::string, TType> modules;
+
+    std::vector<std::string> broadcaster;
+
+    storeModules(lines, modules, broadcaster);
+
+    for(const auto &broad : broadcaster) {
+        std::cout << broad << std::endl;
+    }
+
+    std::cout << "Total number of pulses sent: " << sumTotal(lines, modules) << std::endl;
 
     return 0;
 }
